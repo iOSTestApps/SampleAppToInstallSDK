db75c1d6b3545c7376124837edc7b5d27e8b1b9c
Romain Pouclet <romain.pouclet@gmail.com>
2018-08-13 08:44:32 -0700

    Initial Commit

---
+++ SampleAppToInstallSDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleAppToInstallSDK
+//
+//  Created by Romain Pouclet on 2018-08-13.
+//  Copyright © 2018 Buddybuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
